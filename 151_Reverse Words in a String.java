745321f7328216a3f54b04c25f3156958bf20bd2
Kratos <kratos.omega.hacker@gmail.com>
2016-03-10 22:59:07 -0500

    Update 151_Reverse Words in a String.java

--- 151_Reverse Words in a String.java
+++ 151_Reverse Words in a String.java
@@ -3,7 +3,11 @@ import java.util.ArrayList;
 public class Solution {
     public String reverseWords(String s) {
         s = s.trim();
+        // .trim() is the 1st key point in order to solve this problem
+        // .trim() helps get rid of the whitespaces before and after the string
+        // if the whitespaces exist when during later loop, there will be errors
         String[] origOrder = s.split("\\s+");
+        // \\s+ is used for 1 or more whitespaces, tabs, and lines patterns detection
         ArrayList<String> revertedOrder = new ArrayList<String>();
         String finalString = "";
 
